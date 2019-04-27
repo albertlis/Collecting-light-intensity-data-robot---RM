326836c35aa7ac7b11ca2a2cf6ae76cf79556a9a
Papa Smerf <albert.lis@interia.pl>
2019-04-27 23:50:44 +0200

    Add working platform

--- Kod arduino/stepper/stepper.ino
+++ Kod arduino/stepper/stepper.ino
@@ -1,24 +1,24 @@
 #include <Servo.h>
 
-#define STP 2
-#define MAXSPEED 50.0
 #define PRINTTIME  500
-#define STEPTIME 10
-#define TOLERANCE 10
-#define SERVOTIME 10
+#define STEPTIME 5
+#define TOLERANCE 15
+#define SERVOTIME 20
+#define READTIME 2
 
 #define LEFT_DOWN_PIN A2
 #define RIGHT_DOWN_PIN A0
 #define LEFT_UP_PIN A3
 #define RIGHT_UP_PIN A1
-#define SERVOPIN A8
+#define SERVOPIN A4
 #define DIRECTION 3
+#define STP 2
 
 Servo serwo;
 int16_t Left_Up_Value = 0, Right_Up_Value = 0, Left_Down_Value = 0, Right_Down_Value = 0;
 uint16_t HorizontalDifference = 0, VerticalDifference = 0;
-int16_t StepCounter = 0;
-uint32_t ActualTime = 0, PreviousTimeStep = 0, PreviousTimePrint = 0, PreviousTimeServo = 0;
+int16_t StepCounter = 0, SumRight = 0, SumLeft = 0, DownSum = 0, UpSum = 0;
+uint32_t ActualTime = 0, PreviousTimeStep = 0, PreviousTimePrint = 0, PreviousTimeServo = 0, PreviousTimeRead = 0;
 bool state = LOW;
 uint8_t sposition = 90;
 
@@ -27,43 +27,43 @@ void setup() {
   pinMode(DIRECTION, OUTPUT);
   digitalWrite(DIRECTION, HIGH);
   pinMode(STP, OUTPUT);
-  Serial.begin(500000);
+  Serial.begin(9600);
   serwo.attach(SERVOPIN);
-  //serwo.write(sposition);
+  serwo.write(sposition);
+  delay(500);
 }
 
 void ReadLight();
 void SetServoPosition();
-//void SetStepperPosition();
+void SetStepperPosition();
 
 void loop() {
   ActualTime = millis();
+  if( (ActualTime - PreviousTimeRead) >= READTIME )
+  {
+    PreviousTimeRead = ActualTime;
+    ReadLight();
+    Calculate();
+  }
   if( (ActualTime - PreviousTimeStep) >= STEPTIME )
   {
     PreviousTimeStep = ActualTime;
-    ReadLight();
-    //SetStepperPosition();
+    SetStepperPosition();
   }
   
   if( (ActualTime - PreviousTimeServo) >= SERVOTIME )
   {
     PreviousTimeServo = ActualTime;
-    ReadLight();
     SetServoPosition();
-    //serwo.write(170);
   }
-  
+  /*
   if( (ActualTime - PreviousTimePrint) >= PRINTTIME )
   {
     PreviousTimePrint = ActualTime;
-    //Serial.println(String("") + "Lewa Up: " + Left_Up_Value + " Prawa Up: " + Right_Up_Value + " Left down: " + Left_Down_Value + " Right Down: " + Right_Down_Value);
+    Serial.println(String("") + "Step counter: " + StepCounter/2);
   }
+  */
 }
-
-
-
-
-
 
 void ReadLight()
 {
@@ -72,18 +72,27 @@ void ReadLight()
     Left_Up_Value = analogRead(LEFT_UP_PIN);
     Right_Up_Value = analogRead(RIGHT_UP_PIN);
 }
-/*
+
+void Calculate()
+{
+    SumRight = Right_Up_Value + Right_Down_Value;
+    SumLeft = Left_Up_Value + Left_Down_Value;
+    HorizontalDifference = abs(SumRight - SumLeft);
+    
+    UpSum = Right_Up_Value + Left_Up_Value;
+    DownSum = Left_Down_Value + Right_Down_Value;
+    VerticalDifference = abs(UpSum - DownSum);
+}
 void SetStepperPosition()
 {
-    HorizontalDifference = abs(LeftValue - RightValue);
     if ( HorizontalDifference > TOLERANCE)
     {
-      if (RightValue > LeftValue)
+      if (SumRight > SumLeft)
       {
         digitalWrite(DIRECTION, HIGH);
         ++StepCounter;
       }
-      else if (RightValue < LeftValue)
+      else if (SumRight < SumLeft)
       {
         digitalWrite(DIRECTION, LOW);
         --StepCounter;
@@ -94,12 +103,9 @@ void SetStepperPosition()
     else
       StepCounter = 0;
 }
-*/
+
 void SetServoPosition()
 {
-    int16_t UpSum = Right_Up_Value + Left_Up_Value;
-    int16_t DownSum = Left_Down_Value + Right_Down_Value;
-    VerticalDifference = abs(UpSum - DownSum);
     if ( VerticalDifference > TOLERANCE)
     {
       if (UpSum > DownSum)
@@ -113,5 +119,7 @@ void SetServoPosition()
           serwo.write(++sposition);
       }
     }
-    Serial.println(String("") + "UpSum" + UpSum + " DownSum " + DownSum + " Difference " + VerticalDifference);
+    //Serial.println(String("") + "UpSum" + UpSum + " DownSum " + DownSum + " Vertical Difference " + 
+                  //VerticalDifference + " Horizonatal difference: " + HorizontalDifference + 
+                  //" Step Counter: " + StepCounter);
 }
